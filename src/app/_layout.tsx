5f8ef78c417621e7c2179a187268c9923b982ac3
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2026-04-27 14:41:33 +0200

    Implement splash screen handling, font loading, and configure React Query in layout component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,49 @@
+import { QueryClient } from '@tanstack/react-query';
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { configureReanimatedLogger, ReanimatedLogLevel } from 'react-native-reanimated';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 5 * 60 * 1000,
+			gcTime: 10 * 60 * 1000,
+			refetchOnWindowFocus: true,
+			refetchOnReconnect: true,
+			refetchOnMount: false,
+			retry: 2,
+		},
+	},
+});
+
+configureReanimatedLogger({
+	level: ReanimatedLogLevel.error,
+	strict: true,
+});
+
+SplashScreen.setOptions({
+	duration: 1000,
+	fade: true,
+});
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+	const [loaded, error] = useFonts({
+		SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+	});
+
+	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded && !error) {
+		return null;
+	}
+
 	return <Stack />;
 }
