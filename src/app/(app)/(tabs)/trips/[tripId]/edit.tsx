b1b83b56d3fa4a34df5385cd4da2f4da372791bc
adrianszablowski <adrian.szablowski.kontakt@gmail.com>
2026-04-30 19:25:24 +0200

    refactor: simplify modal presentation for trip creation and editing screens; replace View with SafeAreaView for better layout handling in EditTripScreen

--- src/app/(app)/(tabs)/trips/[tripId]/edit.tsx
+++ src/app/(app)/(tabs)/trips/[tripId]/edit.tsx
@@ -1,14 +1,21 @@
 import { useTheme } from '@/theme/use-theme';
 import { useLocalSearchParams } from 'expo-router';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function EditTripScreen() {
 	const theme = useTheme();
 	const { tripId } = useLocalSearchParams();
 
 	return (
-		<View style={{ flex: 1, backgroundColor: theme.background100 }}>
+		<SafeAreaView style={[styles.root, { backgroundColor: theme.background100 }]} edges={['top']}>
 			<Text>Edit Trip {tripId}</Text>
-		</View>
+		</SafeAreaView>
 	);
 }
+
+const styles = StyleSheet.create({
+	root: {
+		flex: 1,
+	},
+});
--- src/app/(app)/(tabs)/trips/_layout.tsx
+++ src/app/(app)/(tabs)/trips/_layout.tsx
@@ -4,8 +4,8 @@ export default function TripsLayout() {
 	return (
 		<Stack screenOptions={{ headerShown: false }}>
 			<Stack.Screen name='index' />
-			<Stack.Screen name='create' options={{ presentation: 'modal' }} />
-			<Stack.Screen name='[tripId]/edit' options={{ presentation: 'modal' }} />
+			<Stack.Screen name='create' />
+			<Stack.Screen name='[tripId]/edit' />
 			<Stack.Screen name='[tripId]/details' options={{ presentation: 'modal' }} />
 		</Stack>
 	);
